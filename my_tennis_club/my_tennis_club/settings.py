d6a6726e265335e8e3a0fcb4186f54946d21f315
Mh nikkhoofard <nikkhoofard2000@gmail.com>
2023-01-25 20:34:57 +0330

    add new database

--- my_tennis_club/my_tennis_club/settings.py
+++ my_tennis_club/my_tennis_club/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'my_tennis_club.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'A2An2XwizE2aTDLJ',
+        'HOST': 'db.lbtsadykhxtnignocpln.supabase.co',
+        'PORT': '5432'
     }
 }
 
